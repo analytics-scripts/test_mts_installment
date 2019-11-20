e4b561e4ff44c0404e25ad75d4ee572e1a92ce37
analytics-scripts <54020175+analytics-scripts@users.noreply.github.com>
2019-11-20 23:56:51 +0300

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,19 +1,21 @@
-console.log('init');
-start()
+start();
 function start() {
   console.log('start');
   var myInt = setInterval(function () {
     var $buybuttons = $('.product-description .buybutton-credit');
     if ($buybuttons.length) {
       clearInterval(myInt);
+      if($('.basket').first().find('.items-count').text() == '0'){
+      return false;
+      }
       $buybuttons.on('click', function (e) {
-        e.preventDefault()
-        var $closest = $(this).closest('.product-description')
-        $closest.find('.buybutton').first().click()
+        e.preventDefault();
+        var $closest = $(this).closest('.product-description');
+        $closest.find('.buybutton').first().click();
         setTimeout(function () {
-          $closest.find('.basketButton').first().click()
-        }, 100)
-        return false
+          $closest.find('.basketButton').first().click();
+        }, 100);
+        return false;
       });
     }
   }, 100);
@@ -30,7 +32,6 @@ function initInstallmentMethod () {
   $('.delivery  .button-holder input[type="radio"]').on('change', setInstallmentMethod)
 }
 function setInstallmentMethod () {
-  console.log('setInstallmentMethod')
   $('#paysystem-block .button-holder').not('.disabled').each(function(){
     if($(this).find('div:contains("Рассрочка онлайн")').length){
       $(this).find('label').click();
